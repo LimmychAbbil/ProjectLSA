fa3e9c653e7e4bd04da630b9764f6a1efa05c1bd
Limmy <limych24@gmail.com>
2019-05-06 21:36:38 +0300

    0.01a: Init project from Spring Initializr

---
+++ src/main/java/net/lim/ProjectLsaApplication.java
@@ -0,0 +1,13 @@
+package net.lim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectLsaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectLsaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/net/lim/ProjectLsaApplicationTests.java
@@ -0,0 +1,16 @@
+package net.lim;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ProjectLsaApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
